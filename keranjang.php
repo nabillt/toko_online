5e60b1fb70d806bbd193be8c371a17ba4d7a6ae7
nabillt <103652097+nabillt@users.noreply.github.com>
2023-02-20 09:26:41 +0700

    Add files via upload

---
+++ keranjang.php
@@ -0,0 +1,34 @@
+<?php
+include "header.php";
+?>
+<h2>Daftar Produk di Keranjang</h2> 
+    <table class="table table-hover striped">
+<thead>
+<tr>
+    <th>NO</th>
+    <th>Nama produk</th>
+    <th>Jumlah</th>
+    <th>Aksi</th>
+</tr>
+</thead>
+        <tbody>
+            <?php foreach (@$_SESSION['cart'] as $key_produk => $val_produk): ?>
+            <tr>
+            <td>
+                <?=($key_produk + 1) ?></td><td><?= $val_produk['nama_produk'] ?>
+            </td>
+            <td>
+                <?= $val_produk['qty'] ?>
+            </td>
+            <td>
+                <a href="hapus_cart.php?id=<?= $key_produk ?>" class="btn btn-danger"><strong>Hapus</strong></a>
+            </td>
+            </tr> 
+            <?php endforeach ?>
+        </tbody>
+    </table>
+    <a href="checkout.php" class="btn btn-primary">Check Out</a>
+
+<?php
+include "footer.php";
+?>
